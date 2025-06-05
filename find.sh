1990214afe6b93607e6ece1c01c6b3cfcddaf356
Collin Andrews <pizzaTrucks@outlook.com>
2025-06-04 19:30:10 -0500

    Update find.sh

--- find.sh
+++ find.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+# Parameters
+colorR='\e[91m'
+color='\e[92m'
+rst='\e[0m'
 scanRoot=true
 
 # Get inputs
 while getopts "f:d" opt; do
   case $opt in
     f)
-      filename=$OPTARG
+      filename="$OPTARG"
       ;;
     d)
       scanRoot=false
@@ -15,22 +19,22 @@ while getopts "f:d" opt; do
 done
 
 # Inspect file
-if [[ -z $filename ]]; then
-  echo -e "${color}ERROR: No file was provided"
+if [[ -z "$filename" ]]; then
+  echo -e "${color}ERROR: No file was provided${rst}"
   echo -e "Usage: $0"
   echo "Inputs:"
   echo "    -f <file name>"
-  echo "Optional Flags:"
-  echo -e "    -d (scan from current dir (not root dir))\n"
+  echo -e "Optional Flags:"
+  echo -e "    -d (scan from current dir instead of root dir)\n"
   exit 1
 fi
 
 # Display scan info
-echo Searching for file: "$filename"
-echo Scan from root dir: "$scanRoot"
+echo -e "Searching for file: ${colorR}${filename}${rst}"
+echo -e "Scan from root dir: ${colorR}${scanRoot}${color}"
 
 # Find file
-if [[ $scanRoot == true ]]; then
+if [[ "$scanRoot" == true ]]; then
   find / -name "$filename" 2>/dev/null
 else
   find . -name "$filename" 2>/dev/null
